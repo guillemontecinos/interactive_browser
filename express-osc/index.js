97f5b40e260abbdd0a42d298085d29c680a30c1a
gamontec <gamontec@uc.cl>
2019-10-28 12:02:05 -0400

    first prototype

--- express-osc/index.js
+++ express-osc/index.js
@@ -11,9 +11,11 @@ app.use(bodyParser.json())
 // OSC framework
 const options = { send: { port: 11245 } }
 const osc = new OSC({ plugin: new OSC.DatagramPlugin(options) })
+const oscPort = 9005
 
 // array of notes
 let midiBuffer = []
+let currentArray = []
 
 //=============
 // get and post
@@ -25,47 +27,74 @@ app.get("/", function (req, res) {
 });
 
 app.post('/post', function(req, res){
-    // let data = req.body
-    // console.log(data)
-    // edit data and convert it into max msp message format
-    // let dataStr = ''
-    // for (let i = 0; i < req.body.length; i++) {
-    // for (let i = 0; i < 1; i++) {
-    //     for (let j = 0; j < req.body[i].length; j++) {
-    //         dataStr += req.body[i][j]
-    //         if (j < req.body[i].length - 1) {
-    //             dataStr += ' '
-    //         }
-    //         else if(j == req.body[i].length - 1 && i < req.body.length - 1) {
-    //             dataStr +=', '
-    //         }
-    //     }
-    // }
     midiBuffer.push(req.body)
-    console.log(midiBuffer)
-    console.log(midiBuffer.length)
+    // console.log(midiBuffer)
+    console.log('array lenght: ' + midiBuffer.length)
 })
 
-// osc.on('open', () => {
-//     osc.send(new OSC.Message('/data/ ' + JSON.stringify(dataStr)), { port: 9000 })
-// })
-
 osc.on('/time', message => {
+    // console.log(midiBuffer)
     let tempo = message.args[0]
-    console.log(tempo)
     if(tempo == 1){
         // send currentArray[1]
+        if(currentArray.length >= 1){
+            osc.send(new OSC.Message('/off/ 0'), { port: oscPort })
+            osc.send(new OSC.Message('/data/ ' + JSON.stringify(currentArray[1])), { port: oscPort })
+        }
+        else{
+            osc.send(new OSC.Message('/off/ 1'), { port: oscPort })
+            osc.send(new OSC.Message('/data/ nodata'), { port: oscPort })
+        }
     }
     else if (tempo == 2) {
         // send currentArray[2]
+        if(currentArray.length >= 1){
+            osc.send(new OSC.Message('/off/ 0'), { port: oscPort })
+            osc.send(new OSC.Message('/data/ ' + JSON.stringify(currentArray[2])), { port: oscPort })
+        }
+        else{
+            osc.send(new OSC.Message('/off/ 1'), { port: oscPort })
+            osc.send(new OSC.Message('/data/ nodata'), { port: oscPort })
+        }
     }
     else if (tempo == 3) {
         // send currentArray[3]
+        if(currentArray.length >= 1){
+            osc.send(new OSC.Message('/off/ 0'), { port: oscPort })
+            osc.send(new OSC.Message('/data/ ' + JSON.stringify(currentArray[3])), { port: oscPort })
+        }
+        else{
+            osc.send(new OSC.Message('/off/ 1'), { port: oscPort })
+            osc.send(new OSC.Message('/data/ nodata'), { port: oscPort })
+        }
     }
     else if (tempo == 4) {
-        // update currentArray
-        // send currentArray[0]
-        // update midiBuffer
+        if(midiBuffer.length >= 1){
+            // update currentArray
+            currentArray = []
+            for (let i = 0; i < midiBuffer[0].length; i++) {
+                let auxStr = ''
+                for (let j = 0; j < midiBuffer[0][i].length; j++) {
+                    auxStr += midiBuffer[0][i][j]
+                    auxStr += ' '
+                }
+                currentArray.push(auxStr)
+            }
+            console.log('currentArray: ' + currentArray)
+            // send currentArray[0]
+            osc.send(new OSC.Message('/off/ 0'), { port: oscPort })
+            osc.send(new OSC.Message('/data/ ' + JSON.stringify(currentArray[0])), { port: oscPort })
+            // update midiBuffer
+            for (let i = 0; i < midiBuffer.length - 1; i++) {
+                midiBuffer[i] = midiBuffer[i + 1]
+            }
+            midiBuffer.pop()
+            console.log('midiBuffer: ' + midiBuffer)
+        }
+        else{
+            currentArray = []
+            osc.send(new OSC.Message('/off/ 1'), { port: oscPort })
+        }
     }
 })
 
@@ -73,4 +102,4 @@ app.listen(3000, function () {
 	console.log("Example app listening on port 3000!")
 })
 
-osc.open({ port: 9912 }) // bind socket to localhost:9912
+osc.open({ port: 9913 }) // bind socket to localhost:9912
